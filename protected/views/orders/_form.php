258ffdfdec7a0757b1ff2604899e225c00569750
xiaofeng.qiu <664127891@qq.com>
2015-07-10 10:59:53 +0800

    添加orders控制器

---
+++ protected/views/orders/_form.php
@@ -0,0 +1,64 @@
+<?php
+/* @var $this OrdersController */
+/* @var $model Orders */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'orders-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'user_id'); ?>
+		<?php echo $form->textField($model,'user_id',array('size'=>20,'maxlength'=>20)); ?>
+		<?php echo $form->error($model,'user_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'car_id'); ?>
+		<?php echo $form->textField($model,'car_id',array('size'=>20,'maxlength'=>20)); ?>
+		<?php echo $form->error($model,'car_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'date'); ?>
+		<?php echo $form->textField($model,'date'); ?>
+		<?php echo $form->error($model,'date'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'time'); ?>
+		<?php echo $form->textField($model,'time',array('size'=>2,'maxlength'=>2)); ?>
+		<?php echo $form->error($model,'time'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'comment'); ?>
+		<?php echo $form->textField($model,'comment',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'comment'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'created'); ?>
+		<?php echo $form->textField($model,'created'); ?>
+		<?php echo $form->error($model,'created'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
