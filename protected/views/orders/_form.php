3a4affe3c0e1594221591942237b1501d295a8c1
xiaofeng.qiu <664127891@qq.com>
2015-07-10 17:26:49 +0800

    add order model

---
+++ protected/views/orders/_form.php
@@ -0,0 +1,52 @@
+<?php
+/* @var $this OrdersController */
+/* @var $model Orders */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+
+<?php
+/*  $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'orders-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); 
+*/?>
+
+<?php
+/** @var BootActiveForm $form */
+$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+    'id' => 'orders-form',
+    'enableAjaxValidation' => false,
+    'enableClientValidation' => true,
+    'clientOptions' => array(
+        'validateOnSubmit' => true,
+    ),
+    'htmlOptions' => array('class' => 'well'),
+        ));
+?>
+
+	<p class="note">带<span class="required">*</span>为必填项</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+		<?php echo $form->dropDownListRow($model,'car_id',array('car_id' => 'id'),array('class' => 'span4')); ?>
+
+		<?php echo $form->textFieldRow($model,'date',array('class' => 'span4')); ?>
+		
+		<?php echo $form->dropDownListRow($model,'time', array('AM'=>'上午', 'PM'=>'下午'),array('class' => 'span4')); ?>
+
+		<?php echo $form->textFieldRow($model,'comment',array('size'=>60,'maxlength'=>255,'class' => 'span4')); ?>
+
+		<br></br>
+		
+		<?php echo CHtml::submitButton($model->isNewRecord ? '预约' : '修改'); ?>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/orders/_search.php
@@ -0,0 +1,55 @@
+<?php
+/* @var $this OrdersController */
+/* @var $model Orders */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id',array('size'=>20,'maxlength'=>20)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_id'); ?>
+		<?php echo $form->textField($model,'user_id',array('size'=>20,'maxlength'=>20)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'car_id'); ?>
+		<?php echo $form->textField($model,'car_id',array('size'=>20,'maxlength'=>20)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'date'); ?>
+		<?php echo $form->textField($model,'date'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'time'); ?>
+		<?php echo $form->textField($model,'time',array('size'=>2,'maxlength'=>2)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'comment'); ?>
+		<?php echo $form->textField($model,'comment',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'created'); ?>
+		<?php echo $form->textField($model,'created'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
