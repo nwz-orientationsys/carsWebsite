4c20a43780dd6e5db9c62e659adad3e1a9f81a58
Ni <nwz@orientationsys.com>
2015-07-15 11:31:17 +0800

    customer css changes

--- protected/views/customer/update.php
+++ protected/views/customer/update.php
@@ -1,17 +1,5 @@
-<div class="content">
-    <div class="header">
+<?php 
+$this->breadcrumbs = array('我的信息'=>array('index'), '修改我的个人信息');
+?>
 
-        <h1 class="page-title">我的信息</h1>
-    </div>
-
-    <?php
-    /* @var $this CustomerController */
-
-        $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
-            'links'=>array('我的信息'=>array('index'), '修改我的个人信息'),
-        )); ?>
-    <div class="container-fluid">
-  <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
-</div>
-      
-</div>
+<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>>
